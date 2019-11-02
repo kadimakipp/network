209c5e88742265c790539362f9cb64726eb630af
kipp <kadima.kipp@gmail.com>
2019-11-02 22:16:24 +0800

    update losses

--- samhi/losses/losses.md
+++ samhi/losses/losses.md
@@ -20,4 +20,34 @@ torch.nn.CrossEntropyLoss(weight=None, size_average=None, ignore_index=-100, red
 在多分类任务中，经常采用softmax激活函数+交叉熵损失函数，因为**交叉熵描述了两个概率的分布差异**,然而神经网络输出时向量，并不是概率分布的形式。所以需要softmax激活函数
 将一个向量进行归一化成概率分布的形式，在采用交叉熵损失函数计算loss。在回顾Pytorch的`nn.LogSoftmax()`相当与激活函数,`nn.NLLLoss()`是损失函数，将其结合，完整的是否可以叫做softmax
 +交叉熵损失函数呢?
-![crossEntropy](samhi/losses/images/crossEntropy.jpg)
+![crossEntropy](https://github.com/kadimakipp/network/raw/master/samhi/losses/images/crossEntropy.jpg)
+
+### 4.NLLLoss
+```python
+torch.nn.NLLLoss(weight=None, size_average=None, ignore_index=-100, reduce=None, reduction='mean')
+```
+功能不好用语言描述, `loss(input, class)=-input[class]`.for example 三分类任务,`input=[-1.2,2.6,0.5]`,真实标签为2(class=2),则loss为-0.5.就是对应类别输出，区一个负号!.
+实际常用于多分类任务，但是input在输入NLLLoss()之前，需要对input进行log_softmax函数激活，即将input转换成概率分布的形式，并切取对数。
+当带上权值，reduce = True, size_average = True, 其计算公式为:
+![NLLLoss](https://github.com/kadimakipp/network/raw/master/samhi/losses/images/NLLLoss.jpg)
+
+### 5.PoissonNLLLoss
+```python
+torch.nn.PoissonNLLLoss(log_input=True, full=False, size_average=None, eps=1e-08, reduce=None, reduction='mean')
+```
+带泊松分布的负对数似然损失,公式
+```python
+target~Poisson(input)loss(input, target) = input-target*log(input)+log(target!)
+```
+
+### 6.KLDivLoss
+```python
+torch.nn.KLDivLoss(size_average=None, reduce=None, reduction='mean')
+```
+功能:计算input和target之间的的KL离散度
+![KLdiv](https://github.com/kadimakipp/network/raw/master/samhi/losses/images/KLdivLoss.jpg)
+KL离散度又称为相对熵(Relative Entropy),用于描述两个概率分布之间的差异。计算公式(离散时):
+其中p表示真实分布,q表示p的拟合分布,D(P||Q)表示当用概率分布p时,产生的信息损耗。这里的信息损耗可以理解为损失，损失越低，拟合分布q越接近真实分布p。
+同时也可以从另外一个角度上观察这个公式，即计算的时p与q之间的对数差在p上的期望。特别注意,D(p||q)不等于D(q||p),其不具有对称性，因此不能称为K-L距离。
+**信息熵=交叉熵-相对熵**从信息论的角度观察三者，其关系为信息熵=交叉熵-相对熵。在机器学习中，当训练数据固定，最小化相对熵D(p||q)等价于最小化交叉熵H(p,q).
+
--- samhi/losses/losses.py
+++ samhi/losses/losses.py
@@ -18,35 +18,144 @@ import numpy as np
 import torch
 import torchvision
 import torch.nn as nn
+import math
+import warnings
+warnings.filterwarnings("ignore")
+EXP = math.e
 
 def L1_fun():
-    input = torch.ones(3,5, requires_grad=True,dtype=torch.float32)*2
+    input = torch.ones(3,5,dtype=torch.float32)*2
     target = torch.ones(3,5,dtype=torch.float32)
     #becase backward this is a scale
     loss = nn.L1Loss(size_average=None, reduce=None, reduction='mean')
-    return loss(input, target)
+    output = loss(input, target)
+    return output.numpy()
 
 def MSE_fun():
-    input = torch.ones(3, 5, requires_grad=True, dtype=torch.float32) * 2
+    input = torch.ones(3, 5, dtype=torch.float32) * 3
     target = torch.ones(3, 5, dtype=torch.float32)
     # becase backward this is a scale
     loss = nn.MSELoss(size_average=None, reduce=None, reduction='mean')
-    return loss(input, target)
+    output = loss(input, target)
+    return output.numpy()
 
-def CrossEntropyLoss():
-    pass
+def CrossEntropy_fun():
+    loss_f = nn.CrossEntropyLoss(weight=None, size_average=None, ignore_index=-100, reduce=False, reduction='mean')
+    input = torch.ones(3,5)*0.5 #batchsize=3,classes=5,neural=0.5
+    target=torch.from_numpy(np.array([0,1,2])).type(torch.LongTensor)
+    output= loss_f(input, target)
 
-def main(l='L1'):
+
+
+    #numpy
+    batch = input[0].detach().numpy()
+    target_1 = target[0].numpy()
+    #第一项
+    x_class = batch[target_1]
+    #第二项
+    sigma_exp_x = 0
+    for o in batch:
+        sigma_exp_x += pow(EXP, o)
+
+    log_sigma_exp_x = math.log(sigma_exp_x)
+
+    #两项相加
+    loss_1 = -x_class+log_sigma_exp_x
+    print('第一个样本的loss:', loss_1)
+
+
+    #------------------weight
+    weight = torch.from_numpy(np.array([0.1,0.2,0.3,0.4,0.5])).float()
+    loss_weight = nn.CrossEntropyLoss(weight=weight, reduce=False)
+    output2 = loss_weight(input, target)
+
+    print('原始loss值为',output2)
+    print('第一个样本是0类,weight=0.1,输出为1.6094*0.1=', 0.16094)
+    #-------------------ignore_index
+    loss_ignore_1 = nn.CrossEntropyLoss(reduce=False,ignore_index=1)
+    loss_ignore_2 = nn.CrossEntropyLoss(reduce=False,ignore_index=2)
+    loss_1 = loss_ignore_1(input, target)
+    loss_2 = loss_ignore_2(input, target)
+    print('ignore_index=1',loss_1)#注意时class为1，并不是index为1
+    print('ignore_index=2',loss_2)#注意时class为1，并不是index为1
+    return output
+
+def NLL_fun():
+    #-----negative log liklihood loss
+    weight = torch.from_numpy(np.array([0.6, 0.2, 0.2])).float()
+    input = torch.from_numpy(np.array([[0.7,0.2,0.1],[0.4,1.2,0.4]])).float()
+    target = torch.from_numpy(np.array([0, 0])).type(torch.LongTensor)
+
+    loss_f = nn.NLLLoss(weight=weight, size_average=True, reduce=False)
+    output = loss_f(input, target)
+    # pytorch example:
+    m = nn.LogSoftmax(dim=1)
+    loss_f = nn.NLLLoss(reduce=False)
+    #input size NxC=3x5
+    input = torch.randn(3,5)
+    target = torch.tensor([1,0,4])
+    print('-----------1D---------------------')
+    print('input:', m(input))
+    print('target:', target)
+    print('1D:',loss_f(m(input), target))
+
+    # 2D loss example (used, for example, with image input)
+    N, C = 1,2
+    loss_f = nn.NLLLoss(reduce=False)
+    data = torch.randn(N, C, 3, 3)
+    m = nn.LogSoftmax(dim=1)
+    target = torch.empty(N, 3,3,dtype=torch.long).random_(0,C)
+    input = m(data)
+    print('----------------2D-----------------')
+    print('input: ', input)
+    print('target', target)
+    print('2D result:', loss_f(input, target))
+    return output
+
+def PoissonNLL_fun():
+    log_input = torch.randn(5,2)
+    target = torch.randn(5,2)
+    loss_f = nn.PoissonNLLLoss()
+    return loss_f(log_input,target)
+
+def KLdiv_fun():
+    loss_f = nn.KLDivLoss(size_average=False,reduce=False)
+    loss_f_mean = nn.KLDivLoss(size_average=True, reduce=True)
+    input = torch.from_numpy(np.array([[0.1132,0.5477, 0.3390]])).float()
+    target = torch.from_numpy(np.array([[0.8541, 0.0511, 0.0947]])).float()
+
+    output = loss_f(input, target)
+    loss_mean = loss_f_mean(input, target)
+    print('mean: ', loss_mean)
+    #-------------------收到计算样本的第一个元素的loss,注意这里只有一个样本,是element-wise计算的。
+    raw = input[0].numpy()
+    raw_1 = raw[0]
+    target_1 = target[0][0].numpy()
+    loss_1 = target_1*(np.log(target_1) - raw_1)
+    print('第一个样本第一个元素的loss：', loss_1)
+
+    return output
+
+
+
+def main(l='NLL'):
     output = None
     if l in ['L1']:
         output = L1_fun()
     elif l in ['MSE']:
         output = MSE_fun()
-
-    if output is not None:
-        output.backward()
-        output.detach_()
-        print(output.numpy())
+    elif l in ['CrossEntropy']:
+        output = CrossEntropy_fun()
+    elif l in ['NLL']:
+        output = NLL_fun()
+    elif l in ['KLDiv']:
+        output = KLdiv_fun()
+    elif l in ['PoissonNLL']:
+        output = PoissonNLL_fun()
+    else:
+        print('l must be in [L1, MSE, CrossEntropy, NLL, KLDiv, PoissonNLL]')
+    print('\n\n')
+    print(output)
 
 
 
